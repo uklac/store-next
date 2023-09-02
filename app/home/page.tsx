84086ac7104cb1278aac09d71bfb56b07614054c
Lenin Capa <leviskp87@gmail.com>
2023-09-02 10:12:42 -0500

    add styles theme

---
+++ app/home/page.tsx
@@ -0,0 +1,204 @@
+import Alink from 'app/components/Alink';
+
+export default async function Home() {
+  return (
+    <div className="page-content">
+      <div className="container">
+        <div className="row">
+          <h1>Heading H1</h1>
+          <p>
+            Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+            est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+            lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi
+            neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus
+            pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
+          </p>
+          <br />
+          <br/>
+          <h2>Heading H2</h2>
+          <p>
+            Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+            est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+            lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi
+            neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus
+            pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
+          </p>
+          <br />
+          <br/>
+          <h3>Heading H3</h3>
+          <p>
+            Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+            est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+            lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi
+            neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus
+            pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
+          </p>
+          <br />
+          <br/>
+          <h4>Heading H4</h4>
+          <p>
+            Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+            est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+            lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi
+            neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus
+            pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
+          </p>
+          <br />
+          <br/>
+          <h5>Heading H5</h5>
+          <p>
+            Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+            est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+            lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi
+            neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus
+            pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
+          </p>
+          <br />
+          <br/>
+          <h6>Heading H6</h6>
+          <p>
+            Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+            est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+            lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi
+            neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus
+            pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
+          </p>
+          <br />
+          <br/>
+          <h4>Full Width</h4>
+          <p>
+            Morbi purus libero, faucibus adipiscing, commodo quis, gravida id,
+            est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+            lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi
+            neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus
+            pede arcu, dapibus eu, fermentum et, dapibus sed, urna.
+          </p>
+          <br />
+          <br/>
+          <h4>One Half</h4>
+          <div className="row">
+            <div className="col-md-6">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis. Vestibulum volutpat, lacus a ultrices
+                sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare
+                nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus
+                sed, urna.
+              </p>
+            </div>
+
+            <div className="col-md-6">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis. Vestibulum volutpat, lacus a ultrices
+                sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare
+                nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus
+                sed, urna.
+              </p>
+            </div>
+          </div>
+          <br />
+          <br/>
+          <h4>One Third</h4>
+          <div className="row">
+            <div className="col-md-4">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis. Vestibulum volutpat, lacus a ultrices
+                sagittis, mi neque euismod{' '}
+              </p>
+            </div>
+
+            <div className="col-md-4">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis. Vestibulum volutpat, lacus a ultrices
+                sagittis, mi neque euismod{' '}
+              </p>
+            </div>
+
+            <div className="col-md-4">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis. Vestibulum volutpat, lacus a ultrices
+                sagittis, mi neque euismod{' '}
+              </p>
+            </div>
+          </div>
+          <br />
+          <br/>
+          <h4>One Fourth</h4>
+          <div className="row">
+            <div className="col-md-3">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis.{' '}
+              </p>
+            </div>
+
+            <div className="col-md-3">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis.{' '}
+              </p>
+            </div>
+
+            <div className="col-md-3">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis.{' '}
+              </p>
+            </div>
+
+            <div className="col-md-3">
+              <p>
+                Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+                id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed
+                semper lorem at felis.{' '}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="buttons">
+        <div className="row">
+          <div className="col-6 col-lg-4 col-xl-2">
+            <div className="btn-wrap">
+              <span>Rounded Corners Style</span>
+              <a href="#" className="btn btn-primary btn-rounded">
+                Button text
+              </a>
+            </div>
+          </div>
+
+          <div className="col-6 col-lg-4 col-xl-2">
+            <div className="btn-wrap">
+              <span>Square Style</span>
+              <a href="#" className="btn btn-primary">
+                Button text
+              </a>
+            </div>
+          </div>
+
+          <div className="col-6 col-lg-4 col-xl-2">
+            <div className="btn-wrap">
+              <span>Round Style</span>
+              <a href="#" className="btn btn-primary btn-round">
+                Button text
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
