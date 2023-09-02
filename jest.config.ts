7701ad237d9adb36d8f6881abebe0c5380c68b0a
Lenin Capa <leviskp87@gmail.com>
2023-09-02 10:27:47 -0500

    update name

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 /* eslint-disable */
 export default {
-  displayName: 'no',
+  displayName: 'store-next',
   preset: './jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: './coverage/no',
+  coverageDirectory: './coverage/store-next',
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
     '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
