784650c4be0c9bee9880972d886568c3cdf55e33
Lenin Capa <leviskp87@gmail.com>
2023-09-02 14:32:54 -0500

    add to cart button and qty

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -55,6 +55,63 @@ export default async function ProductPage({ params }: { params: any }) {
               </div>
             </div>
           </div>
+          <div className="details-filter-row details-row-size">
+            <label>Qty:</label>
+            <div className="product-details-quantity">
+              <input
+                type="number"
+                id="qty"
+                className="form-control"
+                value="1"
+                min="1"
+                max="10"
+                step="1"
+                data-decimals="0"
+                required
+                style={{ display: 'none' }}
+              />
+              <div className="input-group  input-spinner">
+                <div className="input-group-prepend">
+                  <button
+                    style={{ minWidth: '26px' }}
+                    className="btn btn-decrement btn-spinner"
+                    type="button"
+                  >
+                    <i className="icon-minus"></i>
+                  </button>
+                </div>
+                <input
+                  type="text"
+                  style={{ textAlign: 'center' }}
+                  className="form-control "
+                  required
+                  placeholder=""
+                />
+                <div className="input-group-append">
+                  <button
+                    style={{ minWidth: '26px' }}
+                    className="btn btn-increment btn-spinner"
+                    type="button"
+                  >
+                    <i className="icon-plus"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="product-details-action">
+            <a href="#" className="btn-product btn-cart">
+              <span>add to cart</span>
+            </a>
+            <div className="details-action-wrapper">
+              <a href="#" className="btn-product btn-wishlist" title="Wishlist">
+                <span>Add to Wishlist</span>
+              </a>
+              <a href="#" className="btn-product btn-compare" title="Compare">
+                <span>Add to Compare</span>
+              </a>
+            </div>
+          </div>
         </div>
         <div className="product-details-tab">
           <ul className="nav nav-pills justify-content-center" role="tablist">
