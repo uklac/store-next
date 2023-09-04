17c6600c598b15a82a73b97bceea75227cdfacd9
Lenin Capa <leviskp87@gmail.com>
2023-09-04 15:01:16 -0500

    page products

--- app/apis/products-api.ts
+++ app/apis/products-api.ts
@@ -2,12 +2,10 @@ import { ListProducts, Product } from 'app/interfaces/product';
 import { API_URL, HEADERS_REQUEST } from './constants';
 const PRODUCTS_URL = `${API_URL}/products`;
 
-export async function getProducts(): Promise<ListProducts> {
-  const response = await fetch(PRODUCTS_URL, {
-    // cache: 'no-cache',
-    next: {
-      revalidate: 3600
-    },
+export async function getProducts(page?: number, perPage?: number ): Promise<ListProducts> {
+  const url = `${PRODUCTS_URL}?page=${page || 1}&per_page=${perPage || 12}`;
+  const response = await fetch(url, {
+    cache: 'no-cache',
     headers: HEADERS_REQUEST,
   });
   return await response.json();
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,148 @@
-import React from 'react'
+import { getProducts } from 'app/apis/products-api';
+import { ProductList } from 'components/server';
+import React from 'react';
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  const { products } = await getProducts();
+  const productsList = products.map((product)=>{
+    return {
+      name: product.name,
+      id: product.id,
+      price: product.display_price,
+      image: product.master.images[0]
+    }
+  })
+  
   return (
-    <div>page</div>
-  )
+    <div className="container">
+      <nav>Breadcrumbs here</nav>
+      <div className="page-content">
+        <div className="row">
+          <aside className="col-lg-3">
+            <div className="sidebar sidebar-shop">
+              <div className="widget widget-clean">
+                <label>Filters:</label>
+                <a href="#" className="sidebar-filter-clear">
+                  Clean All
+                </a>
+              </div>
+
+              <div className="widget widget-collapsible">
+                <h3 className="widget-title">
+                  <a
+                    data-toggle="collapse"
+                    href="#widget-1"
+                    role="button"
+                    aria-expanded="true"
+                    aria-controls="widget-1"
+                  >
+                    Category
+                  </a>
+                </h3>
+
+                <div className="collapse show" id="widget-1">
+                  <div className="widget-body">
+                    <div className="filter-items filter-items-count">
+                      <div className="filter-item">
+                        <div className="custom-control custom-checkbox">
+                          <input
+                            type="checkbox"
+                            className="custom-control-input"
+                            id="cat-1"
+                          />
+                          <label className="custom-control-label">
+                            Dresses
+                          </label>
+                        </div>
+                        <span className="item-count">3</span>
+                      </div>
+                      <div className="filter-item">
+                        <div className="custom-control custom-checkbox">
+                          <input
+                            type="checkbox"
+                            className="custom-control-input"
+                            id="cat-2"
+                          />
+                          <label className="custom-control-label">
+                            T-shirts
+                          </label>
+                        </div>
+                        <span className="item-count">0</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="widget widget-collapsible">
+                <h3 className="widget-title">
+                  <a
+                    data-toggle="collapse"
+                    href="#widget-2"
+                    role="button"
+                    aria-expanded="true"
+                    aria-controls="widget-2"
+                  >
+                    Size
+                  </a>
+                </h3>
+              </div>
+
+              <div className="widget widget-collapsible">
+                <h3 className="widget-title">
+                  <a
+                    data-toggle="collapse"
+                    href="#widget-3"
+                    role="button"
+                    aria-expanded="true"
+                    aria-controls="widget-3"
+                  >
+                    Colour
+                  </a>
+                </h3>
+              </div>
+
+              <div className="collapse show" id="widget-3">
+                <div className="widget-body">
+                  <div className="filter-colors">
+                    <a href="#" style={{ background: '#b87145' }}>
+                      <span className="sr-only">Color Name</span>
+                    </a>
+                    <a href="#" style={{ background: '#f0c04a' }}>
+                      <span className="sr-only">Color Name</span>
+                    </a>
+                    <a href="#" style={{ background: '#333333' }}>
+                      <span className="sr-only">Color Name</span>
+                    </a>
+                    <a
+                      href="#"
+                      className="selected"
+                      style={{ background: '#cc3333' }}
+                    >
+                      <span className="sr-only">Color Name</span>
+                    </a>
+                    <a href="#" style={{ background: '#3399cc' }}>
+                      <span className="sr-only">Color Name</span>
+                    </a>
+                    <a href="#" style={{ background: '#669933' }}>
+                      <span className="sr-only">Color Name</span>
+                    </a>
+                    <a href="#" style={{ background: '#f2719c' }}>
+                      <span className="sr-only">Color Name</span>
+                    </a>
+                    <a href="#" style={{ background: '#ebebeb' }}>
+                      <span className="sr-only">Color Name</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </aside>
+          <div className="col-lg-9">
+            <ProductList products={productsList} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
