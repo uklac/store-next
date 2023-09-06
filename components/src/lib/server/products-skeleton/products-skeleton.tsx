09345414dc364b810a297beb0dc062fe67144bdb
Ricardo Capa <ricardocmdn@gmail.com>
2023-09-05 19:40:12 -0500

    changes in skeleton component

--- components/src/lib/server/products-skeleton/products-skeleton.tsx
+++ components/src/lib/server/products-skeleton/products-skeleton.tsx
@@ -1,41 +1,31 @@
-import { ProductList } from '../product-list/product-list'
+interface ProductSkeletonProps {
+  columns?: 2 | 3 | 4;
+  amountProducts?: number;
+}
 
-export async function ProductsSkeleton() {
-  const products = [
-    { 
-      name: '',
-      id: 0,
-      price: '',
-      image: {
-        product_url: ''
-      }
-    },
-    { 
-      name: '',
-      id: 0,
-      price: '',
-      image: {
-        product_url: ''
-      }
-    },
-    { 
-      name: '',
-      id: 0,
-      price: '',
-      image: {
-        product_url: ''
-      }
-    },
-    { 
-      name: '',
-      id: 0,
-      price: '',
-      image: {
-        product_url: ''
-      }
-    }
-  ]
+export async function ProductsSkeleton(props: ProductSkeletonProps) {
+  const { columns, amountProducts } = props;
+
+  const amount = amountProducts || 4;
+
+  const arrayValues = Array.from(Array(amount).keys());
+
+  const grid = {
+    2: 'col-6 col-md-4 col-lg-6',
+    3: 'col-6 col-md-4 col-lg-4',
+    4: 'col-6 col-md-4 col-lg-3',
+  };
+  const gridClass = columns ? grid[columns] : grid[4];
+
   return (
-    <ProductList products={products} />
-  )
+    <div className="products row">
+      {arrayValues.map((product, index) => (
+        <div className={`product-item ${gridClass}`} key={index}>
+          <figure className="product-media">
+            <img alt={''} className="product-image" src={''} />
+          </figure>
+        </div>
+      ))}
+    </div>
+  );
 }
