5e1e001d3dea1876cae3eef04a70962940d2b025
Lenin Capa <leviskp87@gmail.com>
2023-09-08 18:32:13 -0500

    add missing styles by ricardo

--- app-components/client/variant-selection/variant-selection.tsx
+++ app-components/client/variant-selection/variant-selection.tsx
@@ -1,20 +1,28 @@
 'use client';
 import { OptionType } from 'interfaces';
+import styles from './variant-selection.module.scss';
+
 interface VariantSelectionProps {
   title: string;
   optionTypes: OptionType[];
 }
 
+const optionTypeNameToStyleClass: Record<string, string> = {
+  defaultStyle: 'default-styles',
+};
+
 export function VariantSelection(props: VariantSelectionProps) {
   const { title, optionTypes } = props;
+  const optionTypeName = optionTypes[0]?.option_type_name || 'defaultStyle';
+  const styleClass =
+    optionTypeNameToStyleClass[optionTypeName] || 'default-styles';
+
   return (
-    <div className="variant-selection">
-      <h4 className="s">
-        {title}
-      </h4>
-      <div className="option-types">
+    <div className={`${styles['variant-selection']}`}>
+      <h4 className="s">{title}</h4>
+      <div className={`${styles['option-types']}`}>
         {optionTypes.map((optionType, index) => (
-          <div className="option-type" key={index}>
+          <div className={`${styles[styleClass]}`} key={index}>
             {optionType.presentation}
           </div>
         ))}
