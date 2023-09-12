df0efab06ca9bb50e7515f3cd770298b85e2d0d2
Lenin Capa <leviskp87@gmail.com>
2023-09-11 21:29:31 -0500

    add api call

---
+++ apis/account-api.ts
@@ -0,0 +1,28 @@
+import { API_URL } from './constants';
+const ACCOUNT_URL = `${API_URL}/users`;
+type Account = {
+  email: string, 
+  password: string,
+  password_confirmation: string,
+}
+
+export async function createAccount(params: Account): Promise<any> {
+  const url = `${ACCOUNT_URL}`;
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer undefined'
+    },
+    body: JSON.stringify({
+      user: {
+        email: params.email,
+        password: params.password,
+        password_confirmation: params.password
+      }
+    })
+  };
+  const response = await fetch(url, options);
+  return await response.json();
+}
