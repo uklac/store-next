15c19ea64489d49390a599e5a8cdb7999de4a646
Lenin Capa <leviskp87@gmail.com>
2023-09-30 21:22:47 -0500

    show empty state empty cart

--- components/client/cart-view.tsx
+++ components/client/cart-view.tsx
@@ -12,7 +12,6 @@ export async function CartView() {
   const fetchOrder = useCallback(async (orderNumber: string, token: string) => {
     try {
       const order = await getCart(orderNumber, token);
-      debugger
       setOrder(order);
       console.error('response:', order);
     } catch (error) {
@@ -31,29 +30,34 @@ export async function CartView() {
 
   return (
     <div className="cart">
-      {order && (
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-9">
-              <CartLineItems
-                lineItems={order.line_items}
-                orderNumber={order.number}
-                token={guestToken}
-                onChange={()=>{
-                  const storeOrderNumber = localStorage.getItem('order_number');
-                  const storeGuestToken = localStorage.getItem('guest_token');
-                  if (storeOrderNumber && storeGuestToken) {
-                    fetchOrder(storeOrderNumber, storeGuestToken);
-                  }
-                }}
-              />
-            </div>
-            <aside className="col-lg-3">
-              <CartOrderSummary order={order} />
-            </aside>
-          </div>
+      <div className="container">
+        <div className="row">
+          {order && order.line_items.length > 0 ? (
+            <>
+              <div className="col-lg-9">
+                <CartLineItems
+                  lineItems={order.line_items}
+                  orderNumber={order.number}
+                  token={guestToken}
+                  onChange={() => {
+                    const storeOrderNumber =
+                      localStorage.getItem('order_number');
+                    const storeGuestToken = localStorage.getItem('guest_token');
+                    if (storeOrderNumber && storeGuestToken) {
+                      fetchOrder(storeOrderNumber, storeGuestToken);
+                    }
+                  }}
+                />
+              </div>
+              <aside className="col-lg-3">
+                <CartOrderSummary order={order} />
+              </aside>
+            </>
+          ) : (
+            <h4>Tu carrito esta vació</h4>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 }
