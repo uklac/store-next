e1a842a40c71592b3299d988b076d080e390e602
Lenin Capa <leviskp87@gmail.com>
2023-10-07 12:13:13 -0500

    move register form to modal

--- components/client/register-form/register-form.tsx
+++ components/client/register-form/register-form.tsx
@@ -45,151 +45,147 @@ export function RegisterForm(props: Props) {
     }
   };
   return (
-    <div className="login-page bg-image pt-8 pb-8 pt-md-12 pb-md-12 pt-lg-17 pb-lg-17">
-      <div className="container">
-        <div className="form-box">
-          <div className="form-tab">
-            <Tabs selectedTabClassName="show" defaultIndex={0}>
-              <TabList className="nav nav-pills nav-fill">
-                <Tab className="nav-item">
-                  <span className="nav-link">Sign In</span>
-                </Tab>
+    <div className="form-box">
+      <div className="form-tab">
+        <Tabs selectedTabClassName="show" defaultIndex={0}>
+          <TabList className="nav nav-pills nav-fill">
+            <Tab className="nav-item">
+              <span className="nav-link">Sign In</span>
+            </Tab>
 
-                <Tab className="nav-item">
-                  <span className="nav-link">Register</span>
-                </Tab>
-              </TabList>
+            <Tab className="nav-item">
+              <span className="nav-link">Register</span>
+            </Tab>
+          </TabList>
 
-              <div className="tab-content">
-                <TabPanel style={{ paddingTop: '2rem' }}>
-                  <div>
-                    <div className="form-group">
-                      <label htmlFor="singin-email-2">
-                        Username or email address *
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="singin-email-2"
-                        ref={emailLoginRef}
-                        name="singin-email"
-                        required
-                      />
+          <div className="tab-content">
+            <TabPanel style={{ paddingTop: '2rem' }}>
+              <div>
+                <div className="form-group">
+                  <label htmlFor="singin-email-2">
+                    Username or email address *
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="singin-email-2"
+                    ref={emailLoginRef}
+                    name="singin-email"
+                    required
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="singin-password-2">Password *</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="singin-password-2"
+                    name="singin-password"
+                    ref={passwordLoginRef}
+                    required
+                  />
+                </div>
+
+                <div className="form-footer">
+                  <Button onClick={loginSubmit} outline="primary">
+                    <div>
+                      <span>LOG IN</span>
+                      <i className="icon-long-arrow-right"></i>
                     </div>
+                  </Button>
 
-                    <div className="form-group">
-                      <label htmlFor="singin-password-2">Password *</label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="singin-password-2"
-                        name="singin-password"
-                        ref={passwordLoginRef}
-                        required
-                      />
-                    </div>
-
-                    <div className="form-footer">
-                      <Button onClick={loginSubmit} outline="primary">
-                        <div>
-                          <span>LOG IN</span>
-                          <i className="icon-long-arrow-right"></i>
-                        </div>
-                      </Button>
-
-                      <div className="custom-control custom-checkbox">
-                        <input
-                          type="checkbox"
-                          className="custom-control-input"
-                          id="signin-remember-2"
-                        />
-                        <label
-                          className="custom-control-label"
-                          htmlFor="signin-remember-2"
-                        >
-                          Remember Me
-                        </label>
-                      </div>
-
-                      <Link href="/password-recover" className="forgot-link">
-                        Forgot Your Password?
-                      </Link>
-                    </div>
+                  <div className="custom-control custom-checkbox">
+                    <input
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="signin-remember-2"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="signin-remember-2"
+                    >
+                      Remember Me
+                    </label>
                   </div>
-                </TabPanel>
-                <TabPanel>
-                  <div>
-                    <div className="form-group">
-                      <label htmlFor="email">Your email address *</label>
-                      <input
-                        type="email"
-                        className="form-control"
-                        id="email"
-                        ref={emailRef}
-                        name="register-email"
-                        required
-                      />
-                    </div>
 
-                    <div className="form-group">
-                      <label htmlFor="password">Password *</label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        ref={passwordRef}
-                        name="register-password"
-                        required
-                      />
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="password_confirmation">
-                        Password Confirmation:
-                      </label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        id="password_confirmation"
-                        name="register-password"
-                        ref={passwordConfirmationRef}
-                        required
-                      />
-                    </div>
-
-                    <div className="form-footer">
-                      <button
-                        onSubmit={registerSubmit}
-                        onClick={registerSubmit}
-                        type="submit"
-                      >
-                        <div>
-                          <span>SIGN UP</span>
-                          <i className="icon-long-arrow-right"></i>
-                        </div>
-                      </button>
-
-                      <div className="custom-control custom-checkbox">
-                        <input
-                          type="checkbox"
-                          className="custom-control-input"
-                          id="register-policy-2"
-                          required
-                        />
-                        <label
-                          className="custom-control-label"
-                          htmlFor="register-policy-2"
-                        >
-                          I agree to the privacy policy *
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                </TabPanel>
+                  <Link href="/password-recover" className="forgot-link">
+                    Forgot Your Password?
+                  </Link>
+                </div>
               </div>
-            </Tabs>
+            </TabPanel>
+            <TabPanel>
+              <div>
+                <div className="form-group">
+                  <label htmlFor="email">Your email address *</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    ref={emailRef}
+                    name="register-email"
+                    required
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password">Password *</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    ref={passwordRef}
+                    name="register-password"
+                    required
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password_confirmation">
+                    Password Confirmation:
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password_confirmation"
+                    name="register-password"
+                    ref={passwordConfirmationRef}
+                    required
+                  />
+                </div>
+
+                <div className="form-footer">
+                  <button
+                    onSubmit={registerSubmit}
+                    onClick={registerSubmit}
+                    type="submit"
+                  >
+                    <div>
+                      <span>SIGN UP</span>
+                      <i className="icon-long-arrow-right"></i>
+                    </div>
+                  </button>
+
+                  <div className="custom-control custom-checkbox">
+                    <input
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="register-policy-2"
+                      required
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="register-policy-2"
+                    >
+                      I agree to the privacy policy *
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </TabPanel>
           </div>
-        </div>
+        </Tabs>
       </div>
     </div>
   );
