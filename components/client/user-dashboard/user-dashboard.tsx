1e5375a40a1065b60b47d879d70478c1c555c1fe
Ricardo Capa <ricardocmdn@gmail.com>
2023-10-27 16:30:27 -0500

    removing the _getAddressesUser function and adding the currentUser function in the Dashboard Account

--- components/client/user-dashboard/user-dashboard.tsx
+++ components/client/user-dashboard/user-dashboard.tsx
@@ -1,30 +1,26 @@
 'use client';
 
-import Link from 'next/link';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import { useRouter } from 'next/navigation';
 import { useUser } from 'store/hooks/user-hook';
 import { AccountSummary } from 'components/server/account-summary/account-summary';
 import { useEffect, useState } from 'react';
-import { AddressUser, UserOrder } from 'store/slices/user-slice';
+import { UserOrder } from 'store/slices/user-slice';
 import styles from './user-dashboard.module.scss';
 
 export async function UserDashboard() {
   const route = useRouter();
-  const { _logout, _getAddressesUser, _getUserOrders } = useUser();
-  const [address, setAddress] = useState<AddressUser | null>(null);
+  const { _logout, _getUserOrders, currentUser } = useUser();
   const [orders, setOrders] = useState<UserOrder | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const addressData = await _getAddressesUser();
       const ordersData = await _getUserOrders();
-      setAddress(addressData);
       setOrders(ordersData);
     };
 
     fetchData();
-  }, [_getAddressesUser, _getUserOrders]);
+  }, [_getUserOrders]);
 
   async function logout() {
     const { success, error } = await _logout();
@@ -109,33 +105,32 @@ export async function UserDashboard() {
                           <div className="card card-dashboard">
                             <div className="card-body">
                               <h3 className="card-title">Billing Address</h3>
-                              {address &&
-                                address.data &&
-                                address.data?.map((item, index) => (
-                                  <div
-                                    className={`${styles['address-information']}`}
-                                    key={index}
-                                  >
-                                    <div>
-                                      <p>Nombre: </p>
-                                      <p>Dirección 1: </p>
-                                      <p>Dirección 2: </p>
-                                      <p>Teléfono: </p>
-                                      <p>País: </p>
-                                      <p>Ciudad: </p>
-                                      <p>Código Postal: </p>
-                                    </div>
-                                    <div>
-                                      <p>{item.name}</p>
-                                      <p>{item.address1}</p>
-                                      <p>{item.address2}</p>
-                                      <p>{item.phone}</p>
-                                      <p>{item.country.name}</p>
-                                      <p>{item.city}</p>
-                                      <p>{item.zipcode}</p>
-                                    </div>
+                              {currentUser && currentUser.bill_address && (
+                                <div
+                                  className={`${styles['address-information']}`}
+                                >
+                                  <div>
+                                    <p>Nombre: </p>
+                                    <p>Dirección 1: </p>
+                                    <p>Dirección 2: </p>
+                                    <p>Teléfono: </p>
+                                    <p>País: </p>
+                                    <p>Ciudad: </p>
+                                    <p>Código Postal: </p>
                                   </div>
-                                ))}
+                                  <div>
+                                    <p>{currentUser.bill_address.name}</p>
+                                    <p>{currentUser.bill_address.address1}</p>
+                                    <p>{currentUser.bill_address.address2}</p>
+                                    <p>{currentUser.bill_address.phone}</p>
+                                    <p>
+                                      {currentUser.bill_address.country.name}
+                                    </p>
+                                    <p>{currentUser.bill_address.city}</p>
+                                    <p>{currentUser.bill_address.zipcode}</p>
+                                  </div>
+                                </div>
+                              )}
                               {/* <Link href="/">
                                 Edit <i className="icon-edit"></i>
                               </Link> */}
@@ -147,13 +142,35 @@ export async function UserDashboard() {
                           <div className="card card-dashboard">
                             <div className="card-body">
                               <h3 className="card-title">Shipping Address</h3>
-                              <p>
-                                You have not set up this type of address yet.
-                                <br />
-                                <Link href="/">
-                                  Edit <i className="icon-edit"></i>
-                                </Link>
-                              </p>
+                              {currentUser && currentUser.ship_address && (
+                                <div
+                                  className={`${styles['address-information']}`}
+                                >
+                                  <div>
+                                    <p>Nombre: </p>
+                                    <p>Dirección 1: </p>
+                                    <p>Dirección 2: </p>
+                                    <p>Teléfono: </p>
+                                    <p>País: </p>
+                                    <p>Ciudad: </p>
+                                    <p>Código Postal: </p>
+                                  </div>
+                                  <div>
+                                    <p>{currentUser.ship_address.name}</p>
+                                    <p>{currentUser.ship_address.address1}</p>
+                                    <p>{currentUser.ship_address.address2}</p>
+                                    <p>{currentUser.ship_address.phone}</p>
+                                    <p>
+                                      {currentUser.ship_address.country.name}
+                                    </p>
+                                    <p>{currentUser.ship_address.city}</p>
+                                    <p>{currentUser.ship_address.zipcode}</p>
+                                  </div>
+                                </div>
+                              )}
+                              {/* <Link href="/">
+                                Edit <i className="icon-edit"></i>
+                              </Link> */}
                             </div>
                           </div>
                         </div>
